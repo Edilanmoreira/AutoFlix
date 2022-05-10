21ac51ff21a4b4206edd9401583b8165de40a8f1
Edilan Moreira <103012897+Edilanmoreira@users.noreply.github.com>
2022-05-10 15:10:15 -0300

    Add files via upload

---
+++ js/owl/setup.js
@@ -0,0 +1,15 @@
+
+$('.owl-carousel').owlCarousel({
+    items:6,
+    loop:true,
+    margin:0,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
